bc7fd9bd1273b0b683804d6bc5e5a718e83a993a
Techzhihuizhan <yaakov666@163.com>
2024-12-24 19:11:09 +0800

    Site updated: 2024-12-24 19:11:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/24/C/","2024/12/24/C-1/","2024/12/24/Spark/","2024/12/24/Flink/","2024/12/24/Hadoop/","2024/12/24/code/","2024/12/17/hello-world/","2024/12/24/java/","2024/12/24/python/","2024/12/24/scala/"];function toRandomPost(){
+var posts=["2024/12/24/C-1/","2024/12/24/C/","2024/12/24/Flink/","2024/12/24/code/","2024/12/24/Spark/","2024/12/24/Hadoop/","2024/12/17/hello-world/","2024/12/24/java/","2024/12/24/python/","2024/12/24/scala/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
